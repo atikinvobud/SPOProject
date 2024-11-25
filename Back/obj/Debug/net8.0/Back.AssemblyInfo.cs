44fb94cc29c6bf1c63326ddb8fe2770062901c5f
artem777764 <artem200513286@gmail.com>
2024-11-25 11:30:52 +0300

    Добавление молитвы
    Молитвы - очень важно для нашего проекта, ведь работает он чисто на божьей силе.

--- Back/obj/Debug/net8.0/Back.AssemblyInfo.cs
+++ Back/obj/Debug/net8.0/Back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db1bdf61fd6a95bfc8883ced777c1a25ec6ce0b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436a7636a5040af92c0ff2b4e552685d4acdb35e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
