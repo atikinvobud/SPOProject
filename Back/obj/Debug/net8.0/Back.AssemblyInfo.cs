db1bdf61fd6a95bfc8883ced777c1a25ec6ce0b8
atikinvobud <dubov.n5@gmail.com>
2024-11-25 11:25:39 +0300

    a

--- Back/obj/Debug/net8.0/Back.AssemblyInfo.cs
+++ Back/obj/Debug/net8.0/Back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1fcedf59b33125556f2f49ac50892b3b0dc972")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2162864988fa6fc6b58c63e361ec99f7af937d42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
