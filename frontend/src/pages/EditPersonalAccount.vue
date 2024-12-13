344d119080ad733699b6d6de0882407f0eb54a71
ReniX99 <oleg.burkov99@gmail.com>
2024-12-13 22:44:50 +0300

    Исправлены страницы создания и редактирования личного профиля

--- frontend/src/pages/EditPersonalAccount.vue
+++ frontend/src/pages/EditPersonalAccount.vue
@@ -6,23 +6,34 @@
 
   const router = useRouter()
 
+  const cities = ref([])
+  const typeOfSports = ref([])
+
   const personalInfo = ref({})
 
   const fetchPersonalInfo = async () => {
     try {
       const id = localStorage.getItem('id')
 
-      const { data } = await axios.get(`http://localhost:5234/personal-account/${id}`)
+      const [personalInfoResponse, citiesResponse, sportsResponse] = await Promise.all([
+        axios.get(`http://localhost:5234/personal-account/${id}`),
+        axios.get('http://localhost:5234/cities'),
+        axios.get('http://localhost:5234/sports'),
+      ])
+
+      const data = personalInfoResponse.data
       personalInfo.value = {
         surname: data.surname,
         name: data.name,
         gender: data.gender,
         dateOfBirth: data.birthday,
         city: data.city,
-        favouriteSports:
-          data.favouriteSports.length > 0 ? data.favouriteSports.join(', ') : 'Пусто',
-        description: data.description ? data.description : 'Пусто',
+        favouriteSports: data.favouriteSports,
+        description: data.description,
       }
+
+      cities.value = citiesResponse.data
+      typeOfSports.value = sportsResponse.data
     } catch (err) {
       console.log(err)
     }
@@ -30,6 +41,13 @@
 
   const onClickSavePersonalInfo = async () => {
     try {
+      const cityId = cities.value.find((city) => city.name === personalInfo.value.city).id
+      const favouriteSports = typeOfSports.value.filter((typeOfSport) =>
+        personalInfo.value.favouriteSports.includes(typeOfSport.name),
+      )
+
+      const favouriteSportsId = favouriteSports.map((typeOfSport) => typeOfSport.id)
+
       const obj = {
         id: Number(localStorage.getItem('id')),
         surname: personalInfo.value.surname,
@@ -37,10 +55,10 @@
         gender: personalInfo.value.gender,
         dateOfBirth: personalInfo.value.dateOfBirth,
         description: personalInfo.value.description,
-        cityId: 1,
-        favouriteSportsId: [1, 2],
+        cityId: cityId,
+        favouriteSportsId: favouriteSportsId,
       }
-      console.log(obj)
+
       const { data } = await axios.put(`http://localhost:5234/personal-account/edit`, obj)
 
       router.push({ name: 'PersonalAccount' })
@@ -97,7 +115,7 @@
           <div class="flex flex-col gap-1">
             <label for="date-of-birth">Дата рождения</label>
             <input
-              class="w-[300px] rounded-lg bg-gray pl-4 py-3 border-[#D5D5D3]"
+              class="w-[300px] rounded-lg bg-gray pl-4 py-[10px] border-[#D5D5D3]"
               type="date"
               name="date-of-birth"
               placeholder="Дата рождения"
@@ -106,18 +124,40 @@
               v-model="personalInfo.dateOfBirth"
             />
           </div>
+          <div class="flex flex-col gap-1 col-span-2 justify-self-center">
+            <label for="city">Город</label>
+            <select
+              class="w-[300px] rounded-lg bg-gray pl-4 py-[10px] border-[#D5D5D3]"
+              name="city"
+              required
+              v-model="personalInfo.city"
+            >
+              <option v-for="city in cities" :key="city.id" :value="city.name">
+                {{ city.name }}
+              </option>
+            </select>
+          </div>
         </div>
       </article>
       <hr class="line mt-5 mb-5" />
       <article class="flex flex-col gap-4">
         <div class="flex flex-col gap-5">
           <label for="favorite-types-of-sport">Любимые виды спорта</label>
-          <textarea
-            class="rounded-lg text-lg bg-gray pl-4 py-2 border-[#D5D5D3]"
+          <select
+            class="rounded-lg text-lg bg-gray pl-4 py-2 border-[#D5D5D3] basis-2/5"
             type="text"
             name="favorite-types-of-sport"
+            multiple
             v-model="personalInfo.favouriteSports"
-          />
+          >
+            <option
+              v-for="typeOfSport in typeOfSports"
+              :key="typeOfSport.id"
+              :value="typeOfSport.name"
+            >
+              {{ typeOfSport.name }}
+            </option>
+          </select>
         </div>
         <div class="flex flex-col gap-4">
           <label for="about">О себе</label>
