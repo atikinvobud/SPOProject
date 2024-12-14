649a471009f80b31c88f2fe8730c18714c3b999f
ReniX99 <oleg.burkov99@gmail.com>
2024-12-14 23:04:29 +0300

    Добавлен выпадающий список для турниров

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,3 +1,29 @@
+<script setup>
+  import { reactive, ref } from 'vue'
+
+  const isVisibleMainDropdown = ref(false)
+  const isVisibleAdditionalDropdowns = reactive({
+    participant: false,
+    organizer: false,
+  })
+
+  const toggleVisibleMainDropdown = () => {
+    if (isVisibleMainDropdown.value === false) {
+      isVisibleMainDropdown.value = true
+    } else {
+      isVisibleMainDropdown.value = false
+      for (const dropdown in isVisibleAdditionalDropdowns) {
+        isVisibleAdditionalDropdowns[dropdown] = false
+      }
+    }
+  }
+
+  const toggleVisibleAdditionalDropdown = (event) => {
+    const id = event.currentTarget.id
+    isVisibleAdditionalDropdowns[id] = !isVisibleAdditionalDropdowns[id]
+  }
+</script>
+
 <template>
   <aside class="w-52 bg-white pt-[38px] px-4">
     <ul class="flex flex-col gap-7">
@@ -13,9 +39,68 @@
           <span>Найти турнир</span>
         </li>
       </router-link>
-      <li class="flex gap-3 hover:text-light-blue">
-        <img src="/sidebar/cup-logo.svg" alt="cup-logo" />
-        <span>Турниры</span>
+      <li>
+        <div
+          class="hover:text-light-blue flex justify-between cursor-pointer"
+          @click="toggleVisibleMainDropdown"
+        >
+          <div class="flex gap-3">
+            <img src="/sidebar/cup-logo.svg" alt="cup-logo" />
+            <span>Турниры</span>
+          </div>
+          <img
+            class="transition-transform"
+            :class="{ '-rotate-90': isVisibleMainDropdown }"
+            src="/arrow.svg"
+            alt="Arrow"
+          />
+        </div>
+        <ul v-show="isVisibleMainDropdown" class="pl-10 mt-3 flex flex-col gap-3">
+          <li class="">
+            <div
+              class="flex justify-between cursor-pointer hover:text-light-blue"
+              id="participant"
+              @click="toggleVisibleAdditionalDropdown"
+            >
+              <span>Участник</span>
+              <img
+                class="transition-transform"
+                :class="{ '-rotate-90': isVisibleAdditionalDropdowns.participant }"
+                src="/arrow.svg"
+                alt="Arrow"
+              />
+            </div>
+            <ul
+              v-show="isVisibleAdditionalDropdowns.participant"
+              class="mt-2 pl-4 flex flex-col gap-2 text-sm"
+            >
+              <li class="cursor-pointer hover:text-light-blue">Активные</li>
+              <li class="cursor-pointer hover:text-light-blue">Завершённые</li>
+            </ul>
+          </li>
+          <li>
+            <div
+              class="flex justify-between cursor-pointer hover:text-light-blue"
+              id="organizer"
+              @click="toggleVisibleAdditionalDropdown"
+            >
+              <span>Организатор</span>
+              <img
+                class="transition-transform"
+                :class="{ '-rotate-90': isVisibleAdditionalDropdowns.organizer }"
+                src="/arrow.svg"
+                alt="Arrow"
+              />
+            </div>
+            <ul
+              v-show="isVisibleAdditionalDropdowns.organizer"
+              class="mt-2 pl-4 flex flex-col gap-2 text-sm"
+            >
+              <li class="cursor-pointer hover:text-light-blue">Активные</li>
+              <li class="cursor-pointer hover:text-light-blue">Завершённые</li>
+            </ul>
+          </li>
+        </ul>
       </li>
       <li class="flex gap-3 hover:text-light-blue">
         <img src="/sidebar/team-logo.svg" alt="team-logo" />
